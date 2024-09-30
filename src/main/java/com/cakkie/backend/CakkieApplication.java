b38c3ca3a7a7b53bd5a009f709007d871669a7a2
nchieune <hieuncce180986@fpt.edu.vn>
2024-09-30 21:56:52 +0700

    first commit

---
+++ src/main/java/com/cakkie/backend/CakkieApplication.java
@@ -0,0 +1,13 @@
+package com.cakkie.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakkieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakkieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cakkie/backend/CakkieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cakkie.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakkieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
