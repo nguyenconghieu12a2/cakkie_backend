2ec5251c5baccd1880a2c76d78fb3071ce2825d4
nchieune <hieuncce180986@fpt.edu.vn>
2024-10-26 00:22:06 +0700

    em xin loi mng

--- src/main/java/com/cakkie/backend/repository/BannerRepo.java
+++
@@ -1,7 +0,0 @@
-package com.cakkie.backend.repository;
-
-import com.cakkie.backend.model.banners;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BannerRepo extends JpaRepository<banners, Integer> {
-}
