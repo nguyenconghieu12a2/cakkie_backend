bb0594f088b14727fec795f7d0e1871310c2e903
Chinh <133799859+Chinhwinds@users.noreply.github.com>
2024-11-20 11:21:28 +0700

    ready to push

--- src/main/java/com/cakkie/backend/security/WebSecurityConfig.java
+++ src/main/java/com/cakkie/backend/security/WebSecurityConfig.java
@@ -1,7 +1,5 @@
 package com.cakkie.backend.security;
 
-import org.apache.catalina.filters.CorsFilter;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,8 +7,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
